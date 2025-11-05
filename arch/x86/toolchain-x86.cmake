b327ddcd71c81f2c55c3107aa68fefcc8f1ea058
iv-kud <pwasiliy@bk.ru>
2025-11-05 13:36:54 +0700

    cmake: Add x86 cross-compilation toolchain configuration

---
+++ arch/x86/toolchain-x86.cmake
@@ -0,0 +1,36 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_C_COMPILER "i686-elf-gcc")
+set(CMAKE_CXX_COMPILER "i686-elf-g++")
+set(CMAKE_ASM_COMPILER "nasm")
+set(CMAKE_LINKER "i686-elf-ld")
+
+set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_LINKER} <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_LINKER} <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+set(CMAKE_CXX_FLAGS "-std=c++20 -m32 -Wall -ffreestanding -nostdinc -nostdlib -fno-rtti -mgeneral-regs-only")
+set(CMAKE_C_FLAGS "-m32 -Wall -ffreestanding -nostdinc -nostdlib")
+set(CMAKE_ASM_NASM_FLAGS "-f elf32")
+
+set(OS_LINKER_FLAGS
+    "-T${CMAKE_SOURCE_DIR}/kernel/linker.ld"
+    "-m" "elf_i386"
+    "-nostdlib"
+    "-static"
+    "--nmagic"
+)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+string(TOUPPER "${CMAKE_BUILD_TYPE}" BUILD_TYPE_UPPER)
+
+if(BUILD_TYPE_UPPER STREQUAL "DEBUG")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
+    set(CMAKE_ASM_NASM_FLAGS "${CMAKE_ASM_NASM_FLAGS} -g")
+elseif(BUILD_TYPE_UPPER STREQUAL "RELEASE")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -DNDEBUG")
+endif()
